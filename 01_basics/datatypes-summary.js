d9ee815cdc62291a79a0e836ad46c7f63cf1383c
Kushal Sahu <148615970+Kushal9753@users.noreply.github.com>
2025-08-05 14:46:55 +0000

    datatype summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,38 @@
+// Primitive
+
+// 7 types : String, Number, Boolean, Null, undefined, symbole, BigInt
+
+
+
+// Reference (Non primitive)
+
+//Array, Objects, Functions
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedTemp = false
+const outsideTemp = null
+let userEmail;//default undefined
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 3165435645131564562n
+
+const heros = ["shaktiman", "spiderman", "ironman"];
+
+let myObj = {  // objects
+    name: "kushaal",            
+    age: 22,
+
+}
+
+//functions
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof anotherId);
