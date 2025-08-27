dfc98e3fd00c0886c0097ada3a0a41e02d5252a7
Kushal Sahu <148615970+Kushal9753@users.noreply.github.com>
2025-08-27 15:43:36 +0000

    loops

--- 05_iterations/four.js
+++ 05_iterations/four.js
@@ -5,3 +5,25 @@ const myObject = {
     swift: "swift for apple"
 
 }
+
+for (const key in myObject) {
+    // console.log(`${key} shortcut is for ${myObject[key]}`);
+    
+}
+
+const programming = ["js", "rb", "py", "java", "cpp"]
+
+for (const key in programming) {
+    // console.log(programming[key]);
+}
+
+// const map = new Map()
+// map.set('IN',"India")
+// map.set('USA',"United Staes of America")
+// map.set('Fr',"France")
+// map.set('IN',"India")
+
+// for (const key in map) {
+//     console.log(key);
+    
+//     }
--- 05_iterations/three.js
+++ 05_iterations/three.js
@@ -1,5 +1,8 @@
 // for of loop
 
+// ["", "", ""]
+// [{}, {}, {}]
+
 const arr = [1,2,3,4,5]
 
 for (const num of arr) {
